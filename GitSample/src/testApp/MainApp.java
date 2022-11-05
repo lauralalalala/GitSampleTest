b11a49d5c666b26d077abd962d9097e0fdf9c84e
Laura <lamilami1205@gmail.com>
2022-11-05 14:52:03 +0800

    Add MathBranch

--- GitSample/src/testApp/MainApp.java
+++ GitSample/src/testApp/MainApp.java
@@ -4,7 +4,16 @@ public class MainApp {
 
 	public static void main(String[] args) {
 		System.out.println("2nd Version");
-
+		// 組長新增CODE
+		Math cMath=new Math();
+		System.out.println();
+		System.out.println(cMath.add(5,5));
 	}
 
+}
+
+class Math {
+	int add(int x, int y) {
+		return x + y;
+	}
 }
